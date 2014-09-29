90e7821dad3f4f38808a699b84e7d71d47183c63
MDROR <dymmchl@yahoo.com>
2014-09-28 20:11:10 -0400

    added class monster and def initialize

--- monster.rb
+++ monster.rb
@@ -1,6 +1,7 @@
 #Specifications general
 
 #Build a "character generator" using Ruby making use of the majority of topics above, especially classes, arrays/hashes, and methods.
+
 #Specifications specific
 # Executing Ruby programs
 # Flow control (if/while) statements
@@ -13,3 +14,15 @@
 # Print to the screen and accepting user input
 # Performing calculations on data
 # Using Git for basic version control
+
+# define class Monster
+class Monster
+# Every class must contain an initialize method.
+  def initialize(name, role, age, power)
+    # Instance variables
+    @name = name
+    @role = role
+    @age = age.to_i
+    @power = power
+  end
+end
