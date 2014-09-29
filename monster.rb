841b7ae80bbfaabb306cc93ef30ff7a278e8c95c
MDROR <dymmchl@yahoo.com>
2014-09-28 20:08:56 -0400

    add specifications

---
+++ monster.rb
@@ -0,0 +1,15 @@
+#Specifications general
+
+#Build a "character generator" using Ruby making use of the majority of topics above, especially classes, arrays/hashes, and methods.
+#Specifications specific
+# Executing Ruby programs
+# Flow control (if/while) statements
+# Assignment of variables
+# Basic datatypes (String, Float, Integer, Boolean)
+# Collection datatypes (Array, Hash)
+# Writing methods, scope
+# Writing and inheriting classes (Object Oriented Programming)
+# Using pry to debug code
+# Print to the screen and accepting user input
+# Performing calculations on data
+# Using Git for basic version control
